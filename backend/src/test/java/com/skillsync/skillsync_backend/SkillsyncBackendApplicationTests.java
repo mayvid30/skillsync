8e95a2fa95419ff29eba270ac91a2911fba2759f
divyam <mayvid0011@gmail.com>
2025-06-14 18:52:45 +0530

    initial setup

---
+++ backend/src/test/java/com/skillsync/skillsync_backend/SkillsyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsync.skillsync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
